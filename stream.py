1408e9e43e023cbbc9c16c8551926151bf691969
ptmphuong <jennyhiding@gmail.com>
2020-10-09 17:13:00 -0700

    deleted tweeter keys

--- stream.py
+++ stream.py
@@ -3,10 +3,10 @@ from tweepy.streaming import StreamListener
 import json
 import sentiment_mod as s
 
-ckey="ASfenRJcyi4b8r9NjPVzFex8m"
-csecret="WMYSrpfA82DQHNpiTPB6XFkjZgMxFphG1hu4wXKLIoZCZm3aW1"
-atoken="798767325772029952-lcpNI5IOTozzniYRXblWHcfNYYUJoTa"
-asecret="fqWAC8kGtzU1CBrxAcwtdd68VADpwZFf5Y9ivsi00qmkb"
+ckey="put-your-key-here"
+csecret="put-your-key-here"
+atoken="put-your-key-here"
+asecret="put-your-key-here"
 
 # accesstoken = "798767325772029952-lcpNI5IOTozzniYRXblWHcfNYYUJoTa"
 # accesstokensecret = "fqWAC8kGtzU1CBrxAcwtdd68VADpwZFf5Y9ivsi00qmkb"
